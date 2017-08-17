48e6e5817906287e6d4815bf0503008b8d24b2d4
Alonso Vasquez <avasquezchugnas@gmail.com>
2017-08-17 03:25:43 -0500

    Primera version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,12 +5,43 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = []; 
   cargando_productos:boolean = true;
 
   constructor( private http:Http) { 
 
   		this.cargar_productos();
   }
+  public buscar_producto(termino:string){
+
+      //console.log("Buscando producto");
+     // console.log(this.productos.length);
+
+      if (this.productos.length === 0) {
+          
+          this.cargar_productos().then( ()=>{
+            // termino la carga
+          this.filtrar_productos(termino);
+          });
+      }else{
+          this.filtrar_productos(termino);
+      }
+
+  }
+
+  private filtrar_productos(termino:string){
+
+      this.productos_filtrado = [];
+      termino = termino.toLowerCase();
+      this.productos.forEach(prod =>{
+
+        if(prod.categoria.indexOf(termino) >=0 || prod.titulo.toLowerCase().indexOf(termino) >=0){
+            this.productos_filtrado.push(prod);
+        }
+      //  console.log(prod);
+      })
+  }
+
   public cargar_producto(cod:string){
 
     return this.http.get(`https://paginaweb-edaa5.firebaseio.com/productos/${cod}.json`);
@@ -20,18 +51,27 @@ export class ProductosService {
   		
   		this.cargando_productos = true;
 
+      let promesa = new Promise(( resolve, reject)=>{
+
+
   		this.http.get('https://paginaweb-edaa5.firebaseio.com/productos_idx.json')
   			.subscribe( res =>{
 
   				//console.log(res.json());
           
-          setTimeout(()=>{
+        //  setTimeout(()=>{
             this.cargando_productos = false;
             this.productos = res.json();
+            resolve();
 
-          },1500)
+       //   },1500)
   				
   			});
+
+      });
+
+      return promesa;
+
   }
 
 }
